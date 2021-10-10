a8df457e6b983739cf5ef1c26be34038d17d52d2
DELL <nguyendinhthin@gmail.com>
2021-10-10 14:21:22 +0700

    updated toa

---
+++ QuanLyDuongSat/Controller/TauController.cs
@@ -0,0 +1,38 @@
+﻿using QuanLyDuongSat.Enumeration;
+using QuanLyDuongSat.Model.ResponseModel;
+using QuanLyDuongSat.Model.TauModel;
+using QuanLyDuongSat.Model.ToaModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuanLyDuongSat.Controller
+{
+    [RoutePrefix("api/tau")]
+    public class TauController : ApiController
+    {
+        [Route("get-all")]
+        [HttpGet]
+        public ResponseModel GetAll()
+        {
+            using(QuanLyDuongSatDBDataContext db = new QuanLyDuongSatDBDataContext())
+            {
+                var taus = db.Taus.Select(x => new TauGetAllModel()
+                {
+                    MaTau = x.MaTau,
+                    TenTau = x.TenTau
+                }).ToList();
+
+                return new ResponseModel()
+                {
+                    Status = true,
+                    Data = taus
+                };
+            }
+        }
+   
+    }
+}
---
+++ QuanLyDuongSat/Model/TauModel/TauModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyDuongSat.Model.TauModel
+{
+    public class TauGetAllModel
+    {
+        public int MaTau { get; set; }
+        public string TenTau { get; set; }
+    }
+}
